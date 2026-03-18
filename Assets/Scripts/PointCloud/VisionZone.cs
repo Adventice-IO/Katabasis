89921cce97bfa216bc5c4948809ffa04870b9e2f
Ben Kuper <bkuperberg@gmail.com>
2026-03-18 15:12:29 +0100

    fix VisionZone computation

--- Assets/Scripts/PointCloud/VisionZone.cs
+++ Assets/Scripts/PointCloud/VisionZone.cs
@@ -20,23 +20,26 @@ public class VisionZone : MonoBehaviour
 
     public float getWeight()
     {
-        Vector3 localPos = transform.InverseTransformPoint(Camera.main.transform.position);
-        Vector3 absPos = new Vector3(Mathf.Abs(localPos.x), Mathf.Abs(localPos.y), Mathf.Abs(localPos.z));
-        Vector3 size = Vector3.one * 0.5f - Vector3.one * feather;
-        float weight = 1;
-        if (absPos.x > size.x)
+        //inside 1, outside 0, in the feather zone between 1 and 0
+        if (Camera.main == null) return 0f;
+        Vector3 cp = transform.worldToLocalMatrix.MultiplyPoint(Camera.main.transform.position);
+        cp += Vector3.one / 2; // Center the coordinates around (0, 0, 0)
+
+        if (feather == 0)
         {
-            weight *= 1 - (absPos.x - size.x) / feather;
+            return (cp.x > 0 && cp.x < 1 && cp.y > 0 && cp.y < 1 && cp.z > 0 && cp.z < 1) ? 1f : 0f;
         }
-        if (absPos.y > size.y)
-        {
-            weight *= 1 - (absPos.y - size.y) / feather;
-        }
-        if (absPos.z > size.z)
-        {
-            weight *= 1 - (absPos.z - size.z) / feather;
-        }
-        return Mathf.Clamp01(weight);
+
+        float fD = feather / 2;
+
+        float distX1 = Mathf.Max(cp.x / fD, 0);
+        float distX2 = Mathf.Max((1 - cp.x) / fD, 0);
+        float distY1 = Mathf.Max(cp.y / fD, 0);
+        float distY2 = Mathf.Max((1 - cp.y) / fD, 0);
+        float distZ1 = Mathf.Max(cp.z / fD, 0);
+        float distZ2 = Mathf.Max((1 - cp.z) / fD, 0);
+        float minDist = Mathf.Min(distX1, distX2, distY1, distY2, distZ1, distZ2);
+        return minDist;
     }
 
     private void OnDrawGizmos()
@@ -47,6 +50,9 @@ public class VisionZone : MonoBehaviour
 
         Gizmos.color = new Color(0, .8f, .8f, 0.5f);
         Gizmos.DrawWireCube(Vector3.zero, Vector3.one * (1 - feather));
+
+        Gizmos.color = new Color(0, .7f, .7f, getWeight()/3f);
+        Gizmos.DrawCube(Vector3.zero, Vector3.one * (1 - feather * 2));
 
     }
 }
