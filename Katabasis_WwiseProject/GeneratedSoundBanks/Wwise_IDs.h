a51fd95c93668aad92e7c689690f632aa0c4e342
Antoine <antoineboucherikha@gmail.com>
2026-03-20 17:48:39 +0800

    Updated itw events with missing files

--- Katabasis_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ Katabasis_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -20,7 +20,21 @@ namespace AK
         static const AkUniqueID ALEX_3_HIDE_BELOW = 3369146532U;
         static const AkUniqueID ALEX_4_ZERO_TOLERANCE = 1509907605U;
         static const AkUniqueID ALEX_5_SOLITUDE = 2982750171U;
+        static const AkUniqueID AMB_AMBI_EXT_PARK_BIRDS_01 = 561904127U;
+        static const AkUniqueID AMB_AMBI_EXT_PARK_KIDS_01 = 757254316U;
+        static const AkUniqueID AMB_AMBI_EXT_STREET_01 = 3388850435U;
+        static const AkUniqueID AMB_AMBI_EXT_STREET_02 = 3388850432U;
+        static const AkUniqueID AMB_AMBI_EXT_STREET_03 = 3388850433U;
         static const AkUniqueID AMB_AMBI_TEST_01 = 1378078392U;
+        static const AkUniqueID AMB_CITY_A_PEOPLE_CARS = 1305479495U;
+        static const AkUniqueID AMB_CITY_ALARMS_A = 2335628432U;
+        static const AkUniqueID AMB_CITY_ALARMS_B = 2335628435U;
+        static const AkUniqueID AMB_CITY_ALARMS_C = 2335628434U;
+        static const AkUniqueID AMB_CITY_B_PEOPLE_CARS = 1100097896U;
+        static const AkUniqueID AMB_CITY_C_CARS = 2209677657U;
+        static const AkUniqueID AMB_CITY_D_TRUMPET = 780800368U;
+        static const AkUniqueID AMB_CITY_F_CHURCH = 3679105290U;
+        static const AkUniqueID AMB_CITY_G_PEOPLE = 2366823393U;
         static const AkUniqueID CHRIS_2_0_INTRO = 1615727631U;
         static const AkUniqueID CHRIS_2_2_RADIO = 663280590U;
         static const AkUniqueID CHRIS_2_3_DADS_STEPS = 3680887444U;
@@ -43,11 +57,32 @@ namespace AK
         static const AkUniqueID LUCINDA_2_ACCIDENT = 1179470856U;
         static const AkUniqueID MAX_0_INTRO = 1260524331U;
         static const AkUniqueID MAX_1_FEAR_AND_ALERT = 3498491711U;
+        static const AkUniqueID MAX_2_FAMILY = 1382446273U;
         static const AkUniqueID MAX_3_SHARING = 2050142790U;
         static const AkUniqueID MAX_4_DEAD_BODY = 721364632U;
+        static const AkUniqueID MOSES_0_INTRO = 4081052794U;
+        static const AkUniqueID MOSES_1_URBAN_PLANNER = 3896159808U;
+        static const AkUniqueID MOSES_2_RATS = 88498974U;
+        static const AkUniqueID OMILIO_0_INTRO = 581233776U;
+        static const AkUniqueID OMILIO_1_SANTE = 3617820752U;
+        static const AkUniqueID OMILIO_2_PASSENGERS = 2376804707U;
+        static const AkUniqueID OMILIO_3_FAMILY = 2695381743U;
+        static const AkUniqueID ROB_0_INTRO = 407817492U;
+        static const AkUniqueID ROB_1_EVERYDAYLIFE = 3792984946U;
+        static const AkUniqueID ROB_2_METROMAN = 3556172709U;
+        static const AkUniqueID ROB_3_LENFER_TROM = 3798978392U;
         static const AkUniqueID SFX_PORTAL_ACTIVATE = 4131803333U;
         static const AkUniqueID SFX_PORTAL_VALIDATE = 1155651648U;
         static const AkUniqueID SFX_TONE_TEST = 3501277368U;
+        static const AkUniqueID SPIDERMAN_0_INTRO = 838969532U;
+        static const AkUniqueID SPIDERMAN_1_FAMILY_PROBLEMS = 2288458896U;
+        static const AkUniqueID SPIDERMAN_2_MOUV_MUETTE = 1867442572U;
+        static const AkUniqueID STEVE_DUNCAN_0_INTRO = 2763982186U;
+        static const AkUniqueID STEVE_DUNCAN_1_DONT_LIKE_PEOPLE = 3058870194U;
+        static const AkUniqueID STEVE_DUNCAN_2_VILLE_CORPS = 3371055100U;
+        static const AkUniqueID STEVE_DUNCAN_3_WATER = 3669271798U;
+        static const AkUniqueID STEVE_DUNCAN_4_MAFIA = 620272230U;
+        static const AkUniqueID STEVE_DUNCAN_5_POLICE = 1846744149U;
         static const AkUniqueID STEVE_MONA_0_INTRO = 1105975632U;
         static const AkUniqueID STEVE_MONA_1_UNDERGROUND = 1837304316U;
         static const AkUniqueID STEVE_MONA_2_VIETNAM = 331550540U;
@@ -88,6 +123,9 @@ namespace AK
     {
         static const AkUniqueID RTCP_DISTANCE = 1540479560U;
         static const AkUniqueID RTPC_PROGRESSION = 2942853976U;
+        static const AkUniqueID RTPC_VOX_METER_B2 = 598118295U;
+        static const AkUniqueID RTPC_VOX_METER_B3 = 598118294U;
+        static const AkUniqueID RTPC_VOX_METER_B4 = 598118289U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -99,6 +137,7 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID AMB_3D = 2182414867U;
         static const AkUniqueID AMBI = 3537656764U;
         static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
         static const AkUniqueID SFX = 393239870U;
@@ -111,6 +150,7 @@ namespace AK
         static const AkUniqueID DEL_LONG = 223314761U;
         static const AkUniqueID DEL_MID = 2114071087U;
         static const AkUniqueID DEL_SHORT = 3972424543U;
+        static const AkUniqueID REV_DEL_OUTDOOR = 152793239U;
         static const AkUniqueID REV_LONG = 266044713U;
         static const AkUniqueID REV_MID = 618132367U;
         static const AkUniqueID REV_SHORT = 3475958207U;
