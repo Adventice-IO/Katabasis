900fee9333f230a17a049c650117bee88c745706
Antoine <antoineboucherikha@gmail.com>
2026-03-20 12:37:50 +0800

    Interviews wwise events

--- Katabasis_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ Katabasis_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,10 +13,58 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID ALEX0_INTRO = 1126551720U;
+        static const AkUniqueID ALEX1_SUICIDES_1_ = 2650317U;
+        static const AkUniqueID ALEX1_SUICIDES_2_ = 4280839840U;
+        static const AkUniqueID ALEX2_BEBE = 704257172U;
+        static const AkUniqueID ALEX3_HIDEBELOW = 1967762882U;
+        static const AkUniqueID ALEX4_ZEROTOLERANCE = 2941360709U;
+        static const AkUniqueID ALEX5_SOLITUDE = 2784466722U;
         static const AkUniqueID AMB_AMBI_TEST_01 = 1378078392U;
+        static const AkUniqueID CHRIS_2_0_INTRO = 1615727631U;
+        static const AkUniqueID CHRIS_2_2_RADIO = 663280590U;
+        static const AkUniqueID CHRIS_2_3_DADSSTEPS = 2605760609U;
+        static const AkUniqueID CHRIS_3_4_PROFESSIONAL = 3934853553U;
+        static const AkUniqueID CHRIS_4_1_CUPOFTEA = 186635537U;
+        static const AkUniqueID EXITPLAN0_INTRO = 1616039915U;
+        static const AkUniqueID EXITPLAN1_GOODLIFE = 1822874129U;
+        static const AkUniqueID EXITPLAN2_SORRYMUM = 2854405919U;
+        static const AkUniqueID EXITPLAN3_FUCKNYPD = 2233131744U;
+        static const AkUniqueID FREEDOM0_INTRO = 2257758084U;
+        static const AkUniqueID FREEDOM1_MOLEPEOPLE_1_ = 2206044380U;
+        static const AkUniqueID FREEDOM1_MOLEPEOPLE_2_ = 2222821961U;
+        static const AkUniqueID FREEDOM2_EVERYBODYOUT = 913223499U;
+        static const AkUniqueID FREEDOM3_DRUGS = 2583800372U;
+        static const AkUniqueID KIRK0_INTRO = 475139751U;
+        static const AkUniqueID KIRK1_PIPS = 2392294226U;
+        static const AkUniqueID KIRK2_OYSTERS = 1094956408U;
+        static const AkUniqueID LUCINDA0_INTRO = 3888953998U;
+        static const AkUniqueID LUCINDA1_COMMUNITY = 872279846U;
+        static const AkUniqueID LUCINDA2_ACCIDENT = 349083273U;
+        static const AkUniqueID MAX0_INTRO = 675475704U;
+        static const AkUniqueID MAX1_FEARANDALERT = 1056816872U;
+        static const AkUniqueID MAX3_SHARING = 2624729477U;
+        static const AkUniqueID MAX4_DEADBODY = 2917612844U;
         static const AkUniqueID SFX_PORTAL_ACTIVATE = 4131803333U;
         static const AkUniqueID SFX_PORTAL_VALIDATE = 1155651648U;
         static const AkUniqueID SFX_TONE_TEST = 3501277368U;
+        static const AkUniqueID STEVEMONA0_INTRO = 1223614964U;
+        static const AkUniqueID STEVEMONA1_UNDERGROUND = 2910382944U;
+        static const AkUniqueID STEVEMONA2_VIETNAM = 3954657568U;
+        static const AkUniqueID STEVEMONA3_MICA = 2545169113U;
+        static const AkUniqueID STEVEMONA4_CAT_MOUSE = 777783686U;
+        static const AkUniqueID STEVEMONA5_11SEPT = 1416032791U;
+        static const AkUniqueID TERRY0_INTRO = 4204779300U;
+        static const AkUniqueID TERRY1_WHATISUNDERGROUND = 2127044534U;
+        static const AkUniqueID TERRY2_HISTORICAL = 121633952U;
+        static const AkUniqueID TERRY3_MOLEPEOPLE = 2425208859U;
+        static const AkUniqueID TERRY4_FEELING = 1933051926U;
+        static const AkUniqueID TOMMY0_INTRO = 3377493764U;
+        static const AkUniqueID TOMMY1_JUNGLE = 3504550586U;
+        static const AkUniqueID TOMMY2_THEENERGYYOUGIVE = 1120083193U;
+        static const AkUniqueID TOMMY3_EXITSIGN = 1659137940U;
+        static const AkUniqueID TOMMY4_NOSERVICE = 2529036196U;
+        static const AkUniqueID TOMMY5_HYPERSENSITIVE = 1999308203U;
         static const AkUniqueID VOX_TEST_STEVE = 2108038015U;
     } // namespace EVENTS
 
