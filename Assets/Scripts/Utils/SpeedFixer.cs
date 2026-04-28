b95d3caf22533db3fa91f080bf45b4a8ab0f885e
Ugo Arsac <arsacugo@yahoo.fr>
2026-04-28 19:16:42 +0200

    speed fixer implemented

--- Assets/Scripts/Utils/SpeedFixer.cs
+++ Assets/Scripts/Utils/SpeedFixer.cs
@@ -1,16 +1,56 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpeedFixer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool fix;
+    bool lastFix;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(fix != lastFix)
+        {
+            fixSpeeds();
+            lastFix = fix;
+        }
+    }
+
+    void fixSpeeds()
+    {
+        CheckpointContainer[] speeds = GetComponentsInChildren<CheckpointContainer>();
+
+        foreach(var s in speeds)
+        {
+            fixSpeedsFor(s);
+        }
+    }
+
+    void fixSpeedsFor(CheckpointContainer checkpoints)
+    {
+        List<float> newSpeeds = new List<float>();
+        for(var i = 0; i < checkpoints.speedCheckpoints.Count; i++)
+        {
+            if(i == 0 || i == checkpoints.speedCheckpoints.Count - 1)
+            {
+                newSpeeds.Add(checkpoints.speedCheckpoints[i].speed);
+            }
+            else
+            {
+                float prevSpeed = checkpoints.speedCheckpoints[i - 1].speed;
+                float curSpeed = checkpoints.speedCheckpoints[i].speed;
+                newSpeeds.Add(prevSpeed + curSpeed);
+            }
+        }
+
+        for(var i = 0; i < checkpoints.speedCheckpoints.Count; i++)
+        {
+            checkpoints.speedCheckpoints[i].speed = newSpeeds[i];
+        }
     }
 }
